405ac11633622e6af6162f172ba382734def9d30
chakrireddy1609 <chakrireddy1609@gmail.com>
2021-02-17 11:34:43 +0530

    New commit

--- SecondMax.py
+++ SecondMax.py
@@ -2,4 +2,4 @@ numbers_list = [4,6,2,1,99,88]
 
 largest_number = max(numbers_list)
 numbers_list.remove(largest_number)
-print("Second maximum number in the list is : " , max(numbers_list))
+print("Second maximum number in the list is : ",max(numbers_list))
